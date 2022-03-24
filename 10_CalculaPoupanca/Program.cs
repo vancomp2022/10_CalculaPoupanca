6884d460cb8bd45815da7ffed6f2c06475cbbdd8
Vanessa Soares de Morais <vsmdantas@outlook.com>
2022-03-23 21:52:50 -0300

    Adicionar arquivos de projeto.

---
+++ 10_CalculaPoupanca/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _10_CalculaPoupanca{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 10 - Calcula Poupanca ");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+           while (contadorMes <= 12)
+                {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Apos " + contadorMes + " meses, voce tera R$" + valorInvestido);
+
+                //contadorMes = contadorMes + 1;
+                //contadorMes += 1;
+                contadorMes++;
+            }
+
+            /* 0.36% = 0.0036
+            valorInvestido = valorInvestido + (valorInvestido * 0.0036) ;
+            Console.WriteLine("Apos um mes, voce tera R$" + valorInvestido);
+
+            valorInvestido = valorInvestido + (valorInvestido * 0.0036);
+            Console.WriteLine("Apos 2 meses, voce tera R$" + valorInvestido); */
+
+            Console.ReadLine();
+        }
+    }    
+}
